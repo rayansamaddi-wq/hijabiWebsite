226172c8100b57b6e2f8605b70b3ab405aa76c11
Rayan Samaddi <rayansamaddi@gmail.com>
2026-07-24 17:44:01 +0300

    Prepare project for deployment

--- frontEnd/src/constants.js
+++ frontEnd/src/constants.js
@@ -1,6 +1,7 @@
-export const BASE_URL = ' ';
-export const USERS_URL = `/api/v1/users`;
-export const UPLOAD_URL = '/api/v1/upload';
-export const ORDERS_URL = '/api/v1/orders';
-export const PAYMENT_URL = '/api/v1/payment';
-export const PRODUCTS_URL = '/api/v1/products';
+export const BASE_URL = import.meta.env.VITE_API_URL;
+
+export const USERS_URL = `${BASE_URL}/api/v1/users`;
+export const UPLOAD_URL = `${BASE_URL}/api/v1/upload`;
+export const ORDERS_URL = `${BASE_URL}/api/v1/orders`;
+export const PAYMENT_URL = `${BASE_URL}/api/v1/payment`;
+export const PRODUCTS_URL = `${BASE_URL}/api/v1/products`;
