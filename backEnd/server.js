0aa8784498464f1edee1bea0ab633bfd13320fbc
Rayan Samaddi <rayansamaddi@gmail.com>
2026-07-24 16:52:54 +0300

    Fix Express wildcard route for production

--- backEnd/server.js
+++ backEnd/server.js
@@ -46,7 +46,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')));
 
   //any app route that is not api will redirected to index.html
-  app.get('*', (req, res) => {
+  app.get( (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
 } else {
