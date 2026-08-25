e638a16e3b743eb842c727577079a0f60cebf1ed
Rayan Samaddi <rayansamaddi@gmail.com>
2026-08-25 17:01:21 +0300

    debug nodemailer ipv4 dns

--- backEnd/config/email.js
+++ backEnd/config/email.js
@@ -2,9 +2,15 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import nodemailer from 'nodemailer';
+import dns from "dns/promises";
 
 console.log('EMAIL_USER:', process.env.EMAIL_USER);
 console.log('EMAIL_PASS:', process.env.EMAIL_PASS);
+  
+
+const records = await dns.lookup("smtp.gmail.com", { all: true });
+
+console.log("DNS lookup:", records);
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
