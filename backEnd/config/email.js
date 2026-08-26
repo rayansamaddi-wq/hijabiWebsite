94a108381a69466fc60f5f62b28a021e1ee95c9c
Rayan Samaddi <rayansamaddi@gmail.com>
2026-08-26 21:08:53 +0300

    debug nodemailer ipv4 dns

--- backEnd/config/email.js
+++ backEnd/config/email.js
@@ -1,28 +1,32 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-import nodemailer from 'nodemailer';
-
+import nodemailer from "nodemailer";
 import dns from "dns";
 
 dns.setDefaultResultOrder("ipv4first");
 
-console.log('EMAIL_USER:', process.env.EMAIL_USER);
-console.log('EMAIL_PASS:', process.env.EMAIL_PASS);
-  
+// Debug DNS
+dns.lookup("smtp.gmail.com", { all: true }, (err, addresses) => {
+  if (err) {
+    console.error("DNS lookup failed:", err);
+  } else {
+    console.log("DNS lookup:", addresses);
+  }
+});
 
-
-
-console.log("DNS lookup:", records);
+console.log("EMAIL_USER:", process.env.EMAIL_USER);
+console.log("EMAIL_PASS:", process.env.EMAIL_PASS);
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    requireTLS: true,
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
 });
+
 export default transporter;
